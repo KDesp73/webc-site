c9a75ce3936e5b97bcb85c849f477548b775fcd1
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-07-03 02:35:39 +0300

    added site

---
+++ README.md
@@ -0,0 +1,9 @@
+# webc-site
+
+## Build
+
+Build with
+
+```bash
+cc -o site ./site.c -L/usr/local/lib/webc -lwebc -I/usr/local/include/webc/
+```
---
+++ site.c
@@ -0,0 +1,31 @@
+#include "webc-templates/pss.h"
+#include "webc-actions.h"
+
+int main(int argc, char** argv)
+{
+    WebcAction action = WEBC_ParseCliArgs(argc, argv);
+
+    ProjectShowcaseSite site = {
+        .author = "Konstantinos Despoinidis",
+        .year = 2024,
+        .email = "despoinidisk@gmail.com",
+        .title = "WebC",
+        .github_username = "KDesp73",
+        .project.name = "webc",
+        .project.desc= "Write and deploy websites using the C Programming Language.",
+        .project.lang = "C",
+        .project.link = "https://github.com/KDesp73/webc",
+        .project.license = "MIT",
+        .project.version = "v0.0.7",
+        .project.image = "./webc-image.png"
+    };
+
+    Tree tree = WEBC_MakeTree("docs", 
+        WEBC_MakeRoute("/", WEBC_TemplateProjectShowcaseSite(site)),
+        NULL
+    );
+    
+    WEBC_HandleAction(action, tree);
+    
+    return 0;
+}
